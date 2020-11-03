98184affd3b4a3836227ded73f4ee84bcf8a042f
Ashutoshh <ashutoshh@linux.com>
2020-11-03 11:59:08 +0530

    python code updated

--- ashu.py
+++ ashu.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello(): 
 	return "welcome to the Oracle web app with python in DevSecOPs this is change new  !!"
 
+@app.route('/oracle') 
+def hello1():
+	return "Welcome to ORacle Containerization process !!"
+
 
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True) 
