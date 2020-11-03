7a1b63d26bffdeb590e9d09ab6fafdee20b3f311
Ashutoshh <ashutoshh@linux.com>
2020-11-03 10:49:18 +0530

    check for auto build changes

--- ashu.py
+++ ashu.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def hello(): 
-	return "welcome to the Oracle web app with python in DevSecOPs !!"
+	return "welcome to the Oracle web app with python in DevSecOPs this is change new  !!"
 
 
 if __name__ == "__main__": 
